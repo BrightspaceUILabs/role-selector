8c978bfb7c357a1c825dc843bfba385330deb16b
Syeda Azim <Syeda.Azim@D2L.com>
2020-09-22 10:51:35 -0400

    Adding unit test for event

--- test/role-selector.test.js
+++ test/role-selector.test.js
@@ -1,5 +1,5 @@
 import '../role-selector.js';
-import { expect, fixture, html } from '@open-wc/testing';
+import { expect, fixture, html, oneEvent } from '@open-wc/testing';
 import { runConstructor } from '@brightspace-ui/core/tools/constructor-test-helper.js';
 
 describe('d2l-labs-role-selector', () => {
@@ -14,6 +14,24 @@ describe('d2l-labs-role-selector', () => {
 	describe('constructor', () => {
 		it('should construct', () => {
 			runConstructor('d2l-labs-role-selector');
+		});
+	});
+
+	describe('eventing', () => {
+		let el;
+
+		beforeEach(async() => {
+			el = await fixture(html`<d2l-labs-role-selector></d2l-labs-role-selector>`);
+			await new Promise(resolve => setTimeout(resolve, 200)); 
+			await el.updateComplete;
+		});
+
+		it('should fire a d2l-labs-role-selected event when an Select button is clicked', async() => {
+			el.shadowRoot.querySelector('#confirm').click();
+
+			const event = await oneEvent(el, 'd2l-labs-role-selected'); // if no event is fired, this will time out after 2 seconds
+			expect(event.type).to.equal('d2l-labs-role-selected');
+			expect(event.target).to.equal(el);
 		});
 	});
 
