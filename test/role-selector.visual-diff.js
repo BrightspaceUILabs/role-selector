0be3eb294e73a38d24bbf55936f975df90d1fddf
Syeda Azim <Syeda.Azim@D2L.com>
2020-09-29 12:01:06 -0400

    Resolving lint error

--- test/role-selector.visual-diff.js
+++ test/role-selector.visual-diff.js
@@ -8,7 +8,7 @@ describe('d2l-labs-role-selector', () => {
 
 	let browser, page;
 
-	before(async () => {
+	before(async() => {
 		browser = await puppeteer.launch();
 		page = await visualDiff.createPage(browser);
 		await page.setViewport({ width: 700, height: 500, deviceScaleFactor: 2 });
@@ -16,18 +16,18 @@ describe('d2l-labs-role-selector', () => {
 		await page.bringToFront();
 	});
 
-	beforeEach(async () => {
+	beforeEach(async() => {
 		await page.reload(`${visualDiff.getBaseUrl()}/test/role-selector.visual-diff.html`, { waitUntil: ['networkidle0', 'load'] });
 		await visualDiff.resetFocus(page);
 	});
 
-	after(async () => await browser.close());
+	after(async() => await browser.close());
 
-	it('initial', async function () {
+	it('initial', async function() {
 		await visualDiff.screenshotAndCompare(page, this.test.fullTitle());
 	});
 
-	it('role-item-content', async function () {
+	it('role-item-content', async function() {
 		await visualDiff.screenshotAndCompare(page, this.test.fullTitle());
 		await page.$eval('#role-selector', (elem) => {
 			elem._handleDialog();
@@ -35,8 +35,8 @@ describe('d2l-labs-role-selector', () => {
 		await visualDiff.screenshotAndCompare(page, this.test.fullTitle());
 	});
 
-	it('1-role-item-unchecked', async function () {
-		const eventPromise = oneEvent(page, `#role-selector > d2l-labs-role-item`, 'd2l-labs-role-item-selection-change');
+	it('1-role-item-unchecked', async function() {
+		const eventPromise = oneEvent(page, '#role-selector > d2l-labs-role-item', 'd2l-labs-role-item-selection-change');
 		await page.$eval('#role-selector', (elem) => {
 			elem._handleDialog();
 			elem.querySelector('d2l-labs-role-item').selected = false;
@@ -45,7 +45,7 @@ describe('d2l-labs-role-selector', () => {
 		await visualDiff.screenshotAndCompare(page, this.test.fullTitle());
 	});
 
-	it('2-role-items-selected',  async function () {
+	it('2-role-items-selected',  async function() {
 		await page.$eval('#role-selector', (elem) => {
 			elem._handleDialog();
 			elem.querySelector('d2l-labs-role-item').selected = false;
@@ -56,8 +56,8 @@ describe('d2l-labs-role-selector', () => {
 		await visualDiff.screenshotAndCompare(page, this.test.fullTitle());
 	});
 
-	it('all-role-items-unchecked', async function () {
-		const eventPromise = oneEvent(page, `#role-selector > d2l-labs-role-item`, 'd2l-labs-role-item-selection-change');
+	it('all-role-items-unchecked', async function() {
+		const eventPromise = oneEvent(page, '#role-selector > d2l-labs-role-item', 'd2l-labs-role-item-selection-change');
 		await page.$eval('#role-selector', (elem) => {
 			const items = elem._getItems();
 			elem._handleDialog();
