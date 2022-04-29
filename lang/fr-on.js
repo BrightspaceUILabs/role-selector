ba73c533561a87e4d123c78dfc3e64fd87ac4852
JenkinsReader <resupport@desire2learn.com>
2022-04-29 06:23:07 -0400

    Translation Updates

--- lang/fr-on.js
+++ lang/fr-on.js
@@ -1,10 +1,10 @@
 /* eslint quotes: 0 */
 
 export default {
-	"inputLabel": "Roles Included: ",
+	"inputLabel": "Rôles inclus :",
 	"selectRoles": "Sélectionner des rôles",
 	"allRoles": "Tous les rôles",
 	"selectButton": "Sélectionner",
-	"cancelButton": "Cancel",
-	"errorMessage": "You do not have permission to view roles for this org unit"
+	"cancelButton": "Annuler",
+	"errorMessage": "Vous n’êtes pas autorisé à afficher des rôles pour cette unité organisationnelle"
 };
